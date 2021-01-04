541407a43d661d90e4ce0f81f614d4ac340797f0
Andy Augsten <42334738+aaugsten@users.noreply.github.com>
2021-01-04 17:30:38 +0100

    Fix settings.json path

--- app/start.py
+++ app/start.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python
 
 import json
+import os
 import webview
 
 class Api:
     def __init__(self):
-        with open('settings.json') as settings_file:
+        self.settings_dir = os.path.dirname(os.path.abspath(__file__))
+
+        with open('{}/settings.json'.format(self.settings_dir)) as settings_file:
             self.settings = json.load(settings_file)
 
     def example(self, params):
         return 'Hello World {}'.format(params)
 
     def update_settings(self, params):
-        with open('settings.json', 'w') as settings_file:
+        with open('{}/settings.json'.format(self.settings_dir), 'w') as settings_file:
             self.settings[params[0]] = params[1]
             json.dump(self.settings, settings_file)
 

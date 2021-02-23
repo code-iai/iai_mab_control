a0a449e8164f21611704ad6787675fc46d3ddcf3
Dustin Augsten <augsten@uni-bremen.de>
2021-02-23 18:15:26 +0100

    Add parameter for distance tolerance to create_arm_positions Add show_markers Remove usage of PRMstarkConfigDefault and RRTstarkConfigDefault planner libraries Add parameter to move turntable without waiting Move turntable back without waiting after finishing capturing photos at current position"

--- scripts/acquisition.py
+++ scripts/acquisition.py
@@ -6,7 +6,7 @@ from iai_scanning_table_msgs.msg import scanning_tableAction, scanning_tableGoal
 from imp import load_source
 from moveit_commander import MoveGroupCommander, PlanningSceneInterface, RobotCommander
 from rospkg import RosPack
-from visualization_msgs.msg import Marker
+from visualization_msgs.msg import Marker, MarkerArray
 
 import camera
 import math
@@ -17,9 +17,9 @@ import sys
 import tf
 
 def init():
-    global marker_pub
+    global marker_pub, marker_array_pub
     global move_group, turntable
-    global camera_mesh, camera_pos, camera_size, distance_camera_object, num_positions, num_spins, object_size, photobox_size, reach, simulation, test, turntable_pos
+    global camera_mesh, camera_pos, camera_size, distance_camera_object, num_positions, num_spins, object_size, photobox_pos, photobox_size, reach, simulation, test, turntable_pos
 
     rospy.init_node('acquisition')
 
@@ -42,17 +42,19 @@ def init():
     wall_thickness = rospy.get_param('~wall_thickness', 0.04)
 
     marker_pub = rospy.Publisher('visualization_marker', Marker, queue_size=1, latch=True)
+    marker_array_pub = rospy.Publisher('visualization_marker_array', MarkerArray, queue_size=1, latch=True)
 
     move_group = MoveGroupCommander('manipulator')
     move_group.set_max_acceleration_scaling_factor(1.0 if simulation else max_velocity)
     move_group.set_max_velocity_scaling_factor(1.0 if simulation else max_velocity)
+    #move_group.set_planning_time(30.0)
 
     if not simulation:
         planner_ids = move_group.get_interface_description().planner_ids
         if 'manipulator[PRMstarkConfigDefault]' in planner_ids:
-            move_group.set_planner_id('PRMstarkConfigDefault')
+            pass#move_group.set_planner_id('PRMstarkConfigDefault')
         elif 'manipulator[RRTstarkConfigDefault]' in planner_ids:
-            move_group.set_planner_id('RRTstarkConfigDefault')
+            pass#move_group.set_planner_id('RRTstarkConfigDefault')
 
     robot = RobotCommander()
 
@@ -169,6 +171,24 @@ def init():
         ps.pose.position.x = camera_pos[0] + camera_size[0] / 2
         scene.attach_box(eef_link, 'camera_0', ps, camera_size)
 
+def show_markers(poses, lifetime_secs=0.0):
+    marker_array = MarkerArray()
+
+    for i, pose in enumerate(poses):
+        marker = Marker()
+        marker.header.frame_id = 'world'
+        marker.id = i + 1
+        marker.pose = pose
+        marker.scale.x = 0.1
+        marker.scale.y = 0.01
+        marker.scale.z = 0.01
+        marker.color.b = 1.0
+        marker.color.a = 1.0
+        marker.lifetime.secs = lifetime_secs
+        marker_array.markers.append(marker)
+
+    marker_array_pub.publish(marker_array)
+
 def show_marker(pose, lifetime_secs=0.0):
     marker = Marker()
     marker.header.frame_id = 'world'
@@ -204,25 +224,31 @@ def get_orientation_from_pos_to_pos(pos_from, pos_to, y_offset=0.0):
     orientation.w = quaternion[3]
     return orientation
 
-def move_to(position, x_tolerance=0.2):
-    pose = Pose()
-    pose.position.x = position[0]
-    pose.position.y = position[1]
-    pose.position.z = position[2]
-    pose.orientation = get_orientation_from_pos_to_pos(pose.position, turntable_pos, max(camera_size) / 2)
-    show_marker(pose)
+def move_to(positions, x_tolerance=0.2):
+    poses = []
 
-    poses = [pose]
+    pose = Pose()
+    pose.position.x = positions[0][0]
+    pose.position.y = positions[0][1]
+    pose.position.z = positions[0][2]
+    pose.orientation = get_orientation_from_pos_to_pos(pose.position, turntable_pos, max(camera_size) / 1.5)
+    poses.append(pose)
 
     if x_tolerance > 0:
-        for x in numpy.arange(-x_tolerance, x_tolerance + 0.01, 0.01):
-            p = Pose()
-            p.position.x = pose.position.x + x
-            p.position.y = pose.position.y
-            p.position.z = pose.position.z
-            p.orientation = get_orientation_from_pos_to_pos(p.position, turntable_pos, max(camera_size) / 2)
-            poses.append(p)
-        poses.sort(key=lambda pose: abs(pose.position.x))
+        for position in positions:
+            _poses = []
+            for x in arange(-x_tolerance, x_tolerance, 0.05):
+                pose = Pose()
+                pose.position.x = photobox_pos[0] + x
+                pose.position.y = position[1]
+                pose.position.z = position[2]
+                pose.orientation = get_orientation_from_pos_to_pos(pose.position, turntable_pos, max(camera_size) / 1.5)
+                _poses.append(pose)
+            _poses.sort(key=lambda pose: abs(pose.position.x))
+            poses += _poses
+
+    show_markers(poses)
+    show_marker(poses[0])
 
     move_group.set_pose_targets(poses)
     result = move_group.go(wait=True)
@@ -230,15 +256,23 @@ def move_to(position, x_tolerance=0.2):
     move_group.clear_pose_targets()
     return result
 
-def set_turntable_deg(deg, speed=30):
+def set_turntable_deg(deg, wait=True):
     if simulation:
         return True
 
-    turntable.set_speed_deg(speed)
+    turntable.set_speed_deg(30)
     turntable.move_to_deg(deg)
-    return turntable.wait_to_reach_target()
 
-def create_arm_positions(n=15):
+    return turntable.wait_to_reach_target() if wait else True
+    
+
+def arange(start, stop, step):
+    if start == stop:
+        return [start]
+    else:
+        return numpy.arange(start, stop + step, step)
+
+def create_arm_positions(n=15, distance_tolerance=0.0):
     positions = []
     distance = max(camera_size) + distance_camera_object
     div = max(object_size[:2]) / 2 + object_size[2] + math.pi * 2 * distance_camera_object / 4
@@ -249,32 +283,44 @@ def create_arm_positions(n=15):
     max_z = turntable_pos[2] + object_size[2]
 
     for y in numpy.linspace(min_y, max_y, round(n / div * max(object_size[:2]) / 2)):
-        position = [turntable_pos[0] - camera_pos[1], y, max_z + distance]
-        if numpy.linalg.norm(position) <= reach:
-            positions.append(position)
+        _positions = []
+        for z in arange(max_z + distance, max_z + distance + distance_tolerance, 0.1):
+            position = [turntable_pos[0] - camera_pos[1], y, z]
+            if numpy.linalg.norm(position) <= reach:
+                _positions.append(position)
+        if len(_positions):
+            positions.append(_positions)
 
     num = round(n / div * math.pi * 2 * distance_camera_object / 4)
     for i in range(int(num)):
-        position = [turntable_pos[0] - camera_pos[1], max_y + math.sin(math.pi / 2 / num * i) * distance, max_z + math.cos(math.pi / 2 / num * i) * distance]
-        if numpy.linalg.norm(position) <= reach:
-            positions.append(position)
+        _positions = []
+        for d in arange(distance, distance + distance_tolerance, 0.1):
+            position = [turntable_pos[0] - camera_pos[1], max_y + math.sin(math.pi / 2 / num * i) * d, max_z + math.cos(math.pi / 2 / num * i) * d]
+            if numpy.linalg.norm(position) <= reach:
+                _positions.append(position)
+        if len(_positions):
+            positions.append(_positions)
 
     for z in numpy.linspace(max_z, min_z, round(n / div * object_size[2])):
-        position = [turntable_pos[0] - camera_pos[1], max_y + distance, z]
-        if numpy.linalg.norm(position) <= reach:
-            positions.append(position)
+        _positions = []
+        for y in arange(max_y + distance, max_y + distance + distance_tolerance, 0.1):
+            position = [turntable_pos[0] - camera_pos[1], y, z]
+            if numpy.linalg.norm(position) <= reach:
+                _positions.append(position)
+        if len(_positions):
+            positions.append(_positions)
 
     return positions
 
 if __name__ == '__main__':
     init()
 
-    positions = create_arm_positions(num_positions)
+    positions = create_arm_positions(num_positions, 0.0)
     print('Number positions: {}'.format(len(positions)))
 
-    for position in positions:
-        print('Position: {}'.format(position))
-        if move_to(position, photobox_size[0] / 2 - 0.10) and not test:
+    for _positions in positions:
+        print('Position: {}'.format(_positions[0]))
+        if move_to(_positions, photobox_size[0] / 2 - 0.10) and not test:
             for i in range(num_spins):
                 deg = 360 * i / num_spins
                 print('Turntable angle: {} degrees'.format(deg))
@@ -285,7 +331,8 @@ if __name__ == '__main__':
 
                     if file is not None:
                         print('preview: ' + file);
-        print('progress: {}'.format(int(float(positions.index(position) + 1) / len(positions) * 100)))
+            set_turntable_deg(0.0, False)
+        print('progress: {}'.format(int(float(positions.index(_positions) + 1) / len(positions) * 100)))
         rospy.sleep(1)
     camera.exit()
     move_home()
